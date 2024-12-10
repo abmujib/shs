d567c228f95a55cda515f686b3d3a0e53b7a1b3e
Abdul Mujib <sosmed.abdulmujib@gmail.com>
2024-12-11 03:38:12 +0700

    Create Index

---
+++ index.php
@@ -0,0 +1,8 @@
+<h1>Beli Voucher</h1>
+<form action="https://hotspot.steksa.co.id/buatvoucher.php" method="POST">
+<input type="hidden" value="24jam" name="profile">
+<input type="hidden" value="5000" name="saldo">
+<input type="hidden" value="$(mac)" name="mac">
+<button type="submit" name="kirim">BELI
+PAKET</button>
+</form>
